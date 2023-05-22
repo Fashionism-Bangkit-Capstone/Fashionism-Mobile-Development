62f64dc27c07942fca14a60c30bf550b4afc0fad
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-05-22 17:31:13 +0700

    commit to used handler instead of background thread in SplashScreenActivity.kt

--- User App/app/src/main/java/com/fashionism/fashionismclient/ui/splashscreen/SplashScreenActivity.kt
+++ User App/app/src/main/java/com/fashionism/fashionismclient/ui/splashscreen/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package com.fashionism.fashionismclient.ui.splashscreen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.fashionism.fashionismclient.R
 import com.fashionism.fashionismclient.ui.login.LoginActivity
@@ -10,20 +11,13 @@ import com.fashionism.fashionismclient.ui.login.LoginActivity
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // installSplashScreen()
+        installSplashScreen()
         setContentView(R.layout.activity_splash_screen)
 
-        val background = object : Thread() {
-            override fun run() {
-                try {
-                    Thread.sleep(2000)
-                    val loginIntent = Intent(baseContext, LoginActivity::class.java)
-                    startActivity(loginIntent)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
-        }
-        background.start()
+        Handler().postDelayed(Runnable {
+            val i = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 3000) // waktu delay dalam milidetik sebelum pindah ke MainActivity
     }
 }
