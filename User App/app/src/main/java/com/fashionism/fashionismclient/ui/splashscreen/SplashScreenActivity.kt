2b529f8967cd4e536785dbb514931e7840ccb65c
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-05-22 16:16:50 +0700

    delete new splash screen that implemented in app

--- User App/app/src/main/java/com/fashionism/fashionismclient/ui/splashscreen/SplashScreenActivity.kt
+++ User App/app/src/main/java/com/fashionism/fashionismclient/ui/splashscreen/SplashScreenActivity.kt
@@ -1,12 +1,29 @@
 package com.fashionism.fashionismclient.ui.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.fashionism.fashionismclient.R
+import com.fashionism.fashionismclient.ui.login.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // installSplashScreen()
         setContentView(R.layout.activity_splash_screen)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(2000)
+                    val loginIntent = Intent(baseContext, LoginActivity::class.java)
+                    startActivity(loginIntent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
     }
 }
